9ce9f21bf3a62a7a33476ad302b98ae05c225470
Sergiy Kalyna <seryjk452@gmail.com>
2024-03-24 11:09:00 +0200

    MS-user Added user mapper.

---
+++ infrastructure/database/src/main/java/com/militarystore/user/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.militarystore.user.mapper;
+
+import com.militarystore.entity.user.User;
+import com.militarystore.entity.user.model.Gender;
+import com.militarystore.entity.user.model.Role;
+import org.jooq.Record;
+import org.jooq.RecordMapper;
+import org.springframework.stereotype.Component;
+
+import static com.militarystore.jooq.Tables.USERS;
+import static java.util.Objects.isNull;
+
+@Component
+public class UserMapper implements RecordMapper<Record, User> {
+
+    @Override
+    public User map(Record userRecord) {
+        if (isNull(userRecord)) {
+            return null;
+        }
+
+        return User.builder()
+            .id(userRecord.get(USERS.ID))
+            .login(userRecord.get(USERS.LOGIN))
+            .password(userRecord.get(USERS.PASSWORD))
+            .firstName(userRecord.get(USERS.FIRST_NAME))
+            .secondName(userRecord.get(USERS.SECOND_NAME))
+            .email(userRecord.get(USERS.EMAIL))
+            .phone(userRecord.get(USERS.PHONE))
+            .gender(Gender.valueOf(userRecord.get(USERS.GENDER)))
+            .role(Role.valueOf(userRecord.get(USERS.ROLE)))
+            .birthdayDate(userRecord.get(USERS.BIRTHDAY_DATE))
+            .isBanned(userRecord.get(USERS.IS_BANNED))
+            .build();
+    }
+}
---
+++ infrastructure/database/src/test/java/com/militarystore/user/mapper/UserMapperTest.java
@@ -0,0 +1,64 @@
+package com.militarystore.user.mapper;
+
+import com.militarystore.entity.user.User;
+import com.militarystore.entity.user.model.Gender;
+import com.militarystore.entity.user.model.Role;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static com.militarystore.jooq.Tables.USERS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserMapperTest {
+
+    private static final DSLContext dslContext = DSL.using(SQLDialect.POSTGRES);
+
+    private UserMapper userMapper;
+
+    @BeforeEach
+    void setUp() {
+        userMapper = new UserMapper();
+    }
+
+    @Test
+    void map_whenRecordIsNull_shouldReturnNull() {
+        assertThat(userMapper.map(null)).isNull();
+    }
+
+    @Test
+    void map_shouldCorrectMapRecord() {
+        var userRecord = dslContext.newRecord(USERS.fields());
+        userRecord.setValue(USERS.ID, 1);
+        userRecord.setValue(USERS.LOGIN, "login");
+        userRecord.setValue(USERS.PASSWORD, "password");
+        userRecord.setValue(USERS.FIRST_NAME, "first_name");
+        userRecord.setValue(USERS.SECOND_NAME, "second_name");
+        userRecord.setValue(USERS.EMAIL, "email");
+        userRecord.setValue(USERS.PHONE, "phone");
+        userRecord.setValue(USERS.GENDER, "MALE");
+        userRecord.setValue(USERS.ROLE, "ADMIN");
+        userRecord.setValue(USERS.BIRTHDAY_DATE, LocalDate.EPOCH);
+        userRecord.setValue(USERS.IS_BANNED, false);
+
+        var expectedUser = User.builder()
+            .id(1)
+            .login("login")
+            .password("password")
+            .firstName("first_name")
+            .secondName("second_name")
+            .email("email")
+            .phone("phone")
+            .gender(Gender.MALE)
+            .role(Role.ADMIN)
+            .birthdayDate(LocalDate.EPOCH)
+            .isBanned(false)
+            .build();
+
+        assertThat(userMapper.map(userRecord)).isEqualTo(expectedUser);
+    }
+}
