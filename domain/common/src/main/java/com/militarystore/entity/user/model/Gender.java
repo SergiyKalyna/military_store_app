977e652456c68f20988c0a5222c0c7d007f0f1f0
Sergiy Kalyna <seryjk452@gmail.com>
2024-03-24 10:14:28 +0200

    MS-user Updated Gender enum.

--- domain/common/src/main/java/com/militarystore/entity/user/model/Gender.java
+++ domain/common/src/main/java/com/militarystore/entity/user/model/Gender.java
@@ -1,14 +1,5 @@
 package com.militarystore.entity.user.model;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
 public enum Gender {
-    MALE("Male"),
-    FEMALE("Female"),
-    OTHER("Other");
-
-    private final String description;
+    MALE, FEMALE, OTHER
 }
