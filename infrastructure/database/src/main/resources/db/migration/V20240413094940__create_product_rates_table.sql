1cc72c3060aefda9189ff57c18531aacca4b7745
Sergiy Kalyna <seryjk452@gmail.com>
2024-04-13 10:52:22 +0300

    MS-rates Created table 'product_rates'.

---
+++ infrastructure/database/src/main/resources/db/migration/V20240413094940__create_product_rates_table.sql
@@ -0,0 +1,10 @@
+create table product_rates
+(
+    id          int primary key,
+    user_id     int not null,
+    product_id  int not null,
+    rate        double precision not null,
+    unique (user_id, product_id),
+    foreign key (user_id) references users (id),
+    foreign key (product_id) references products (id)
+);
