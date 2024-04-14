a1a59fb3aed3a82a35e426a63c683f4a010010de
Sergiy Kalyna <seryjk452@gmail.com>
2024-04-14 16:42:20 +0300

    MS-rates Updated migration.

--- infrastructure/database/src/main/resources/db/migration/V20240413094940__create_product_rates_table.sql
+++ infrastructure/database/src/main/resources/db/migration/V20240413094940__create_product_rates_table.sql
@@ -1,9 +1,9 @@
 create table product_rates
 (
-    id          int primary key,
-    user_id     int not null,
-    product_id  int not null,
-    rate        double precision not null,
+    id         serial           not null primary key,
+    user_id    int              not null,
+    product_id int              not null,
+    rate       double precision not null,
     unique (user_id, product_id),
     foreign key (user_id) references users (id),
     foreign key (product_id) references products (id)
