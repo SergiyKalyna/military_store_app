c7d47a50856f6e2c064bd5a675be189f7613516c
Sergiy Kalyna <seryjk452@gmail.com>
2024-04-21 09:55:18 +0300

    MS-feedbacks Added product feedback port.

---
+++ domain/common/src/main/java/com/militarystore/port/out/product/ProductFeedbackPort.java
@@ -0,0 +1,22 @@
+package com.militarystore.port.out.product;
+
+import com.militarystore.entity.product.ProductFeedback;
+
+import java.util.List;
+
+public interface ProductFeedbackPort {
+
+    Integer saveFeedback(ProductFeedback productFeedback);
+
+    void updateFeedback(ProductFeedback productFeedback);
+
+    void deleteFeedback(Integer feedbackId);
+
+    void deleteFeedbacksByProductId(Integer productId);
+
+    ProductFeedback getFeedbackById(Integer feedbackId);
+
+    List<ProductFeedback> getFeedbacksByProductId(Integer productId);
+
+    boolean isFeedbackExist(Integer feedbackId);
+}
---
+++ infrastructure/database/src/main/java/com/militarystore/product/feedback/ProductFeedbackAdapter.java
@@ -0,0 +1,54 @@
+package com.militarystore.product.feedback;
+
+import com.militarystore.entity.product.ProductFeedback;
+import com.militarystore.port.out.product.ProductFeedbackPort;
+import com.militarystore.product.mapper.ProductFeedbackMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ProductFeedbackAdapter implements ProductFeedbackPort {
+
+    private final ProductFeedbackRepository productFeedbackRepository;
+    private final ProductFeedbackMapper productFeedbackMapper;
+
+    @Override
+    public Integer saveFeedback(ProductFeedback productFeedback) {
+        return productFeedbackRepository.saveFeedback(productFeedback);
+    }
+
+    @Override
+    public void updateFeedback(ProductFeedback productFeedback) {
+        productFeedbackRepository.updateFeedback(productFeedback);
+    }
+
+    @Override
+    public void deleteFeedback(Integer feedbackId) {
+        productFeedbackRepository.deleteFeedback(feedbackId);
+    }
+
+    @Override
+    public void deleteFeedbacksByProductId(Integer productId) {
+        productFeedbackRepository.deleteFeedbacksByProductId(productId);
+    }
+
+    @Override
+    public ProductFeedback getFeedbackById(Integer feedbackId) {
+        return productFeedbackRepository.getFeedbackById(feedbackId).map(productFeedbackMapper);
+    }
+
+    @Override
+    public List<ProductFeedback> getFeedbacksByProductId(Integer productId) {
+        return productFeedbackRepository.getFeedbacksByProductId(productId).stream()
+            .map(productFeedbackMapper::map)
+            .toList();
+    }
+
+    @Override
+    public boolean isFeedbackExist(Integer feedbackId) {
+        return productFeedbackRepository.isFeedbackExist(feedbackId);
+    }
+}
