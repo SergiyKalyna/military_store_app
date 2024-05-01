cd36d5ab1cfcf8d419a3e9c2283d3422b7ac6db6
Sergiy Kalyna <seryjk452@gmail.com>
2024-05-01 13:31:36 +0300

    MS-basket Added product basket controller.

---
+++ infrastructure/web/src/main/java/com/militarystore/basket/ProductBasketController.java
@@ -0,0 +1,61 @@
+package com.militarystore.basket;
+
+import com.militarystore.converter.ProductBasketConverter;
+import com.militarystore.model.response.ProductBasketResponse;
+import com.militarystore.port.in.ProductBasketUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products/basket")
+public class ProductBasketController {
+
+    private final ProductBasketUseCase productBasketUseCase;
+    private final ProductBasketConverter productBasketConverter;
+
+    @PostMapping("/user/{userId}/product-details-id/{product-details-id}")
+    public void addProductToBasket(
+        @PathVariable("product-details-id") Integer productStockDetailsId,
+        @PathVariable("userId") Integer userId,
+        @RequestParam("quantity") Integer quantity
+    ) {
+        productBasketUseCase.addProductToBasket(productStockDetailsId, userId, quantity);
+    }
+
+    @PutMapping("/user/{userId}/product-details-id/{product-details-id}")
+    public void updateProductQuantityInBasket(
+        @PathVariable("product-details-id") Integer productStockDetailsId,
+        @PathVariable("userId") Integer userId,
+        @RequestParam("quantity") Integer quantity
+    ) {
+        productBasketUseCase.updateProductQuantityInBasket(productStockDetailsId, userId, quantity);
+    }
+
+    @DeleteMapping("/user/{userId}/product-details-id/{product-details-id}")
+    public void deleteProductFromBasket(
+        @PathVariable("product-details-id") Integer productStockDetailsId,
+        @PathVariable("userId") Integer userId
+    ) {
+        productBasketUseCase.deleteProductFromBasket(productStockDetailsId, userId);
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public void deleteUserProductsFromBasket(@PathVariable("userId") Integer userId) {
+        productBasketUseCase.deleteUserProductsFromBasket(userId);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ProductBasketResponse getUserBasketProducts(@PathVariable("userId") Integer userId) {
+       var basketProducts = productBasketUseCase.getUserBasketProducts(userId);
+
+       return productBasketConverter.convertToProductBasketResponse(basketProducts);
+    }
+}
---
+++ infrastructure/web/src/test/java/com/militarystore/basket/ProductBasketControllerTest.java
@@ -0,0 +1,87 @@
+package com.militarystore.basket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.militarystore.converter.ProductBasketConverter;
+import com.militarystore.entity.basket.ProductInBasket;
+import com.militarystore.model.dto.basket.ProductInBasketDto;
+import com.militarystore.model.response.ProductBasketResponse;
+import com.militarystore.port.in.ProductBasketUseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductBasketController.class)
+@ContextConfiguration(classes = ProductBasketController.class)
+class ProductBasketControllerTest {
+
+    private static final Integer PRODUCT_DETAILS_ID = 1;
+    private static final Integer USER_ID = 1;
+
+    @MockBean
+    private ProductBasketUseCase productBasketUseCase;
+
+    @MockBean
+    private ProductBasketConverter productBasketConverter;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void addProductToBasket() throws Exception {
+        mockMvc.perform(
+            post("/products/basket/user/{userId}/product-details-id/{product-details-id}", USER_ID, PRODUCT_DETAILS_ID)
+            .param("quantity", "1"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateProductQuantityInBasket() throws Exception {
+        mockMvc.perform(
+            put("/products/basket/user/{userId}/product-details-id/{product-details-id}", USER_ID, PRODUCT_DETAILS_ID)
+            .param("quantity", "1"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteProductFromBasket() throws Exception {
+        mockMvc.perform(
+            delete("/products/basket/user/{userId}/product-details-id/{product-details-id}", USER_ID, PRODUCT_DETAILS_ID)
+            .param("quantity", "1"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteUserProductsFromBasket() throws Exception {
+        mockMvc.perform(delete("/products/basket/user/{userId}", USER_ID))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void getUserBasketProducts() throws Exception {
+        var productsInBasket = List.of(ProductInBasket.builder().build());
+        var productBasketResponse = new ProductBasketResponse(List.of(ProductInBasketDto.builder().build()), 11);
+
+        when(productBasketUseCase.getUserBasketProducts(USER_ID)).thenReturn(productsInBasket);
+        when(productBasketConverter.convertToProductBasketResponse(productsInBasket)).thenReturn(productBasketResponse);
+
+        mockMvc.perform(get("/products/basket/user/{userId}", USER_ID))
+            .andExpect(status().isOk())
+            .andExpect(content().json(objectMapper.writeValueAsString(productBasketResponse)));
+    }
+}
