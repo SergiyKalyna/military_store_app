b46f3fa1256e937caab89c0abb50a0cca72a0c13
Sergiy Kalyna <seryjk452@gmail.com>
2024-05-02 17:09:11 +0300

    MS-discount Added user discount controller.

---
+++ infrastructure/web/src/test/java/com/militarystore/discount/UserDiscountControllerTest.java
@@ -0,0 +1,63 @@
+package com.militarystore.discount;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.militarystore.converter.discount.DiscountConverter;
+import com.militarystore.entity.discount.Discount;
+import com.militarystore.model.dto.DiscountDto;
+import com.militarystore.port.in.discount.DiscountUseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserDiscountController.class)
+@ContextConfiguration(classes = {UserDiscountController.class})
+class UserDiscountControllerTest {
+
+    private static final int USER_ID = 1;
+    private static final String DISCOUNT_CODE = "discountCode";
+
+    @MockBean
+    private DiscountUseCase discountUseCase;
+
+    @MockBean
+    private DiscountConverter discountConverter;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createUserDiscountCode() throws Exception {
+        when(discountUseCase.createUserDiscountCode(USER_ID)).thenReturn(DISCOUNT_CODE);
+
+        mockMvc.perform(post("/users/discount/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(DISCOUNT_CODE));
+    }
+
+    @Test
+    void getUserDiscounts() throws Exception {
+        var discountDto = DiscountDto.builder().build();
+        var discount = Discount.builder().build();
+
+        when(discountUseCase.getUserDiscounts(USER_ID)).thenReturn(List.of(discount));
+        when(discountConverter.toDto(discount)).thenReturn(discountDto);
+
+        mockMvc.perform(get("/users/discount/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(objectMapper.writeValueAsString(List.of(discountDto))));
+    }
+}
