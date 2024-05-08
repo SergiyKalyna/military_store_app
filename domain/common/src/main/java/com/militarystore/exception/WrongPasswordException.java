c670d73293ed9174d2b5b34749566e30ec779af0
Sergiy Kalyna <seryjk452@gmail.com>
2024-05-08 13:04:22 +0300

    MS-security updated http status for WrongPasswordException.

--- domain/common/src/main/java/com/militarystore/exception/WrongPasswordException.java
+++ domain/common/src/main/java/com/militarystore/exception/WrongPasswordException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 public class WrongPasswordException extends MilitaryStoreRuntimeException {
 
     public WrongPasswordException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
+        super(message, HttpStatus.UNAUTHORIZED);
     }
 }
