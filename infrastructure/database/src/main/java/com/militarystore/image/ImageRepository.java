15165849de07c9138f649fd5b66d2585c6af4498
Sergiy Kalyna <seryjk452@gmail.com>
2024-06-13 20:34:42 +0300

    MS-images Added image repository.

---
+++ infrastructure/database/src/main/java/com/militarystore/image/ImageRepository.java
@@ -0,0 +1,52 @@
+package com.militarystore.image;
+
+import com.militarystore.jooq.tables.records.ImagesRecord;
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.militarystore.jooq.Tables.IMAGES;
+
+@Repository
+@RequiredArgsConstructor
+public class ImageRepository {
+
+    private final DSLContext dslContext;
+
+    public void saveImages(List<ImagesRecord> imagesRecords) {
+        dslContext.batchInsert(imagesRecords).execute();
+    }
+
+    public List<String> getImagesByProductId(Integer productId) {
+        return dslContext.select(IMAGES.GOOGLE_DRIVE_ID)
+            .from(IMAGES)
+            .where(IMAGES.PRODUCT_ID.eq(productId))
+            .orderBy(IMAGES.ORDINAL_NUMBER)
+            .fetch(IMAGES.GOOGLE_DRIVE_ID);
+    }
+
+    public Map<Integer, String> getPrimaryImagesByProductIds(List<Integer> productIds) {
+        return dslContext.select(IMAGES.GOOGLE_DRIVE_ID)
+            .from(IMAGES)
+            .where(IMAGES.PRODUCT_ID.in(productIds))
+            .and(IMAGES.ORDINAL_NUMBER.eq(1))
+            .fetchMap(IMAGES.PRODUCT_ID, IMAGES.GOOGLE_DRIVE_ID);
+    }
+
+    public void deleteImagesByProductId(Integer productId) {
+        dslContext.deleteFrom(IMAGES)
+            .where(IMAGES.PRODUCT_ID.eq(productId))
+            .execute();
+    }
+
+    public boolean isImageExist(Integer productId) {
+        return dslContext.fetchExists(
+            dslContext.selectOne()
+                .from(IMAGES)
+                .where(IMAGES.PRODUCT_ID.eq(productId))
+        );
+    }
+}
