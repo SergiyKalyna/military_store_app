2ab229d9b57a850cb3a3dd5cb208be5325fe8b43
Sergiy Kalyna <seryjk452@gmail.com>
2024-06-16 10:14:27 +0300

    MS-images Removed unused method.

--- infrastructure/database/src/main/java/com/militarystore/product/mapper/ProductMapper.java
+++ infrastructure/database/src/main/java/com/militarystore/product/mapper/ProductMapper.java
@@ -1,16 +1,12 @@
 package com.militarystore.product.mapper;
 
 import com.militarystore.entity.product.Product;
-import com.militarystore.entity.product.ProductFeedback;
-import com.militarystore.entity.product.ProductStockDetails;
 import com.militarystore.entity.product.model.ProductSizeGridType;
 import com.militarystore.entity.product.model.ProductTag;
 import com.militarystore.jooq.tables.records.ProductsRecord;
 import org.jooq.Record;
 import org.jooq.RecordMapper;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 import static com.militarystore.jooq.Tables.PRODUCTS;
 import static com.militarystore.product.ProductRepository.AVG_RATE;
@@ -27,29 +23,6 @@ public class ProductMapper implements RecordMapper<Record, Product> {
             .tag(ProductTag.valueOf(productRecord.get(PRODUCTS.PRODUCT_TAG)))
             .isInStock(productRecord.get(PRODUCTS.IS_IN_STOCK))
             .avgRate(productRecord.get(AVG_RATE, Double.class))
-            .build();
-    }
-
-    public Product map(
-        ProductsRecord productRecord,
-        List<ProductStockDetails> productStockDetails,
-        double avgRate,
-        List<ProductFeedback> productFeedbacks,
-        boolean isProductInUserWishlist
-    ) {
-        return Product.builder()
-            .id(productRecord.get(PRODUCTS.ID))
-            .name(productRecord.get(PRODUCTS.NAME))
-            .description(productRecord.get(PRODUCTS.DESCRIPTION))
-            .price(productRecord.get(PRODUCTS.PRICE))
-            .subcategoryId(productRecord.get(PRODUCTS.SUBCATEGORY_ID))
-            .sizeGridType(ProductSizeGridType.valueOf(productRecord.get(PRODUCTS.SIZE_GRID_TYPE)))
-            .tag(ProductTag.valueOf(productRecord.get(PRODUCTS.PRODUCT_TAG)))
-            .isInStock(productRecord.get(PRODUCTS.IS_IN_STOCK))
-            .stockDetails(productStockDetails)
-            .avgRate(avgRate)
-            .feedbacks(productFeedbacks)
-            .isProductInUserWishlist(isProductInUserWishlist)
             .build();
     }
 
