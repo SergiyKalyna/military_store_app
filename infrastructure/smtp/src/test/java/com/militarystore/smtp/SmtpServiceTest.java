61d2078fea6baaf556fba73a313c329e35be2561
Sergiy Kalyna <seryjk452@gmail.com>
2024-06-25 21:21:59 +0300

    MS-inbucket Refactored code style.

--- infrastructure/smtp/src/test/java/com/militarystore/smtp/SmtpServiceTest.java
+++ infrastructure/smtp/src/test/java/com/militarystore/smtp/SmtpServiceTest.java
@@ -52,53 +52,53 @@ class SmtpServiceTest {
     }
 
     private String getContent() {
-        return "<!DOCTYPE html>\n" +
-            "<html>\n" +
-            "<head>\n" +
-            "    <meta charset=\"UTF-8\">\n" +
-            "    <title>Military Store Order Details</title>\n" +
-            "    <style>\n" +
-            "        body {\n" +
-            "            font-family: Arial, sans-serif;\n" +
-            "            line-height: 1.6;\n" +
-            "        }\n" +
-            "        .container {\n" +
-            "            width: 80%;\n" +
-            "            margin: auto;\n" +
-            "            padding: 20px;\n" +
-            "            border: 1px solid #ddd;\n" +
-            "            border-radius: 8px;\n" +
-            "            background-color: #f9f9f9;\n" +
-            "        }\n" +
-            "        .header {\n" +
-            "            text-align: center;\n" +
-            "            margin-bottom: 20px;\n" +
-            "        }\n" +
-            "        .footer {\n" +
-            "            margin-top: 20px;\n" +
-            "            text-align: center;\n" +
-            "        }\n" +
-            "        .footer a {\n" +
-            "            color: #007BFF;\n" +
-            "            text-decoration: none;\n" +
-            "        }\n" +
-            "    </style>\n" +
-            "</head>\n" +
-            "<body>\n" +
-            "<div class=\"container\">\n" +
-            "    <div class=\"header\">\n" +
-            "        <h2>Military Store Order Details</h2>\n" +
-            "    </div>\n" +
-            "    <p>Dear Mr Smith,</p>\n" +
-            "    <p>We are glad to notify you that your order is ready.</p>\n" +
-            "    <p>Thank you for your order.</p>\n" +
-            "    <p>If you have any questions, please email us at <a href=\"mailto:militarystorenotificator@gmail.com\">militarystorenotificator@gmail.com</a>.</p>\n" +
-            "    <div class=\"footer\">\n" +
-            "        <p>Best regards,<br>\n" +
-            "            Administration of the Military Store</p>\n" +
-            "    </div>\n" +
-            "</div>\n" +
-            "</body>\n" +
-            "</html>\n";
+        return "<!DOCTYPE html>\n"
+            + "<html>\n"
+            + "<head>\n"
+            + "    <meta charset=\"UTF-8\">\n"
+            + "    <title>Military Store Order Details</title>\n"
+            + "    <style>\n"
+            + "        body {\n"
+            + "            font-family: Arial, sans-serif;\n"
+            + "            line-height: 1.6;\n"
+            + "        }\n"
+            + "        .container {\n"
+            + "            width: 80%;\n"
+            + "            margin: auto;\n"
+            + "            padding: 20px;\n"
+            + "            border: 1px solid #ddd;\n"
+            + "            border-radius: 8px;\n"
+            + "            background-color: #f9f9f9;\n"
+            + "        }\n"
+            + "        .header {\n"
+            + "            text-align: center;\n"
+            + "            margin-bottom: 20px;\n"
+            + "        }\n"
+            + "        .footer {\n"
+            + "            margin-top: 20px;\n"
+            + "            text-align: center;\n"
+            + "        }\n"
+            + "        .footer a {\n"
+            + "            color: #007BFF;\n"
+            + "            text-decoration: none;\n"
+            + "        }\n"
+            + "    </style>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "<div class=\"container\">\n"
+            + "    <div class=\"header\">\n"
+            + "        <h2>Military Store Order Details</h2>\n"
+            + "    </div>\n"
+            + "    <p>Dear Mr Smith,</p>\n"
+            + "    <p>We are glad to notify you that your order is ready.</p>\n"
+            + "    <p>Thank you for your order.</p>\n"
+            + "    <p>If you have any questions, please email us at <a href=\"mailto:militarystorenotificator@gmail.com\">militarystorenotificator@gmail.com</a>.</p>\n"
+            + "    <div class=\"footer\">\n"
+            + "        <p>Best regards,<br>\n"
+            + "            Administration of the Military Store</p>\n"
+            + "    </div>\n"
+            + "</div>\n"
+            + "</body>\n"
+            + "</html>\n";
     }
 }
